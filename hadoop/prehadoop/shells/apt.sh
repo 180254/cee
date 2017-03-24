952e3cac7077b9803497c34a09e249d9a2aeec6c
Adrian Pędzwiatr <180254@edu.p.lodz.pl>
2017-03-24 23:57:06 +0100

    prehadoop: missing required folders

--- hadoop/prehadoop/shells/apt.sh
+++ hadoop/prehadoop/shells/apt.sh
@@ -2,7 +2,7 @@
 
 echo "------- shell apt -------"
 
-mv /home/vagrant/archives/*  /var/cache/apt/archives/
+mv /home/vagrant/archives/* /var/cache/apt/archives/
 
 apt-get install -y rsync ssh openjdk-8-jre dsh htop
 
--- hadoop/prehadoop/shells/aptupdate.sh
+++ hadoop/prehadoop/shells/aptupdate.sh
@@ -2,7 +2,7 @@
 
 echo "------- shell aptupdate -------"
 
-mv /home/vagrant/lists/*     /var/lib/apt/lists/
+mv /home/vagrant/lists/* /var/lib/apt/lists/
 
 add-apt-repository ppa:openjdk-r/ppa
 
