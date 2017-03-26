700a7bf7e7bac61c6fea8f67e9dfde43570cfcbe
Adrian Pędzwiatr <180254@edu.p.lodz.pl>
2017-03-26 16:19:37 +0200

    prehadoop: fix creating dirs on hdfs

--- hadoop/prehadoop/up.sh
+++ hadoop/prehadoop/up.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 vagrant up
-vagrant ssh hadoop-master -c "/usr/local/hadoop/bin/hdfs namenode -format"
-vagrant ssh hadoop-master -c "/usr/local/hadoop/bin/hdfs dfs -mkdir /user"
-vagrant ssh hadoop-master -c "/usr/local/hadoop/bin/hdfs dfs -mkdir /user/vagrant"
-vagrant ssh hadoop-master -c "/usr/local/hadoop/sbin/start-dfs.sh"
-vagrant ssh hadoop-master -c "/usr/local/hadoop/sbin/start-yarn.sh"
-vagrant ssh hadoop-master -c "/usr/bin/jps"
-vagrant ssh hadoop-slave1 -c "/usr/bin/jps"
+
+if [ $? -eq 0 ]; then
+    vagrant ssh hadoop-master -c "/usr/local/hadoop/bin/hdfs namenode -format"
+    vagrant ssh hadoop-master -c "/usr/local/hadoop/sbin/start-dfs.sh"
+    vagrant ssh hadoop-master -c "/usr/local/hadoop/sbin/start-yarn.sh"
+
+    vagrant ssh hadoop-master -c "/usr/local/hadoop/bin/hdfs dfs -mkdir /user"
+    vagrant ssh hadoop-master -c "/usr/local/hadoop/bin/hdfs dfs -mkdir /user/vagrant"
+
+    vagrant ssh hadoop-master -c "/usr/bin/jps"
+    vagrant ssh hadoop-slave1 -c "/usr/bin/jps"
+
+else
+    echo "up.sh failed, code: $?"
+fi
 
