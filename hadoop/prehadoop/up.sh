883f4e14a55796196aac66b9dfef8cb7e8e34e17
Adrian Pędzwiatr <180254@edu.p.lodz.pl>
2017-03-26 15:11:43 +0200

    prehadoop: comments; list jvm processed at the end

--- hadoop/prehadoop/up.sh
+++ hadoop/prehadoop/up.sh
@@ -6,4 +6,6 @@ vagrant ssh hadoop-master -c "/usr/local/hadoop/bin/hdfs dfs -mkdir /user"
 vagrant ssh hadoop-master -c "/usr/local/hadoop/bin/hdfs dfs -mkdir /user/vagrant"
 vagrant ssh hadoop-master -c "/usr/local/hadoop/sbin/start-dfs.sh"
 vagrant ssh hadoop-master -c "/usr/local/hadoop/sbin/start-yarn.sh"
+vagrant ssh hadoop-master -c "/usr/bin/jps"
+vagrant ssh hadoop-slave1 -c "/usr/bin/jps"
 
