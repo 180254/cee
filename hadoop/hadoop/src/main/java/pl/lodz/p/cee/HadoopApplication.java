b80d4f5fda92824a12135a0b0196c833721253ee
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2017-03-28 13:01:40 +0200

    hadoop: project init

---
+++ hadoop/hadoop/src/main/java/pl/lodz/p/cee/HadoopApplication.java
@@ -0,0 +1,12 @@
+package pl.lodz.p.cee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadoopApplication.class, args);
+	}
+}
