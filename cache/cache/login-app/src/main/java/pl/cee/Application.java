f4b9b9578aafcf9a7f76aaeb9b9d59576dc9a07a
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2017-04-03 23:27:02 +0200

    cache: ignite successfully start

---
+++ cache/cache/login-app/src/main/java/pl/cee/Application.java
@@ -0,0 +1,24 @@
+package pl.cee;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("classpath:config-ignite.xml")
+public class Application {
+
+    public static void main(String[] args) {
+        System.setProperty("IGNITE_QUIET", "false");
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Ignite ignite(IgniteConfiguration igniteConfiguration) {
+        return Ignition.start(igniteConfiguration);
+    }
+}
