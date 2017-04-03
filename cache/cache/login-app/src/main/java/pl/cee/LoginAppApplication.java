b10be964c50ffeed7a37238e85b5d29fb1776fb0
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2017-04-03 20:25:23 +0200

    cache: login-app project init

---
+++ cache/cache/login-app/src/main/java/pl/cee/LoginAppApplication.java
@@ -0,0 +1,12 @@
+package pl.cee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginAppApplication.class, args);
+    }
+}
