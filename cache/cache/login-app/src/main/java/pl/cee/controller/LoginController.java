a125987c97e01fc1f9dda86ca85ac2babc190623
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2017-04-04 15:45:40 +0200

    cache: refactoring; expiry policy

--- cache/cache/login-app/src/main/java/pl/cee/controller/LoginController.java
+++ cache/cache/login-app/src/main/java/pl/cee/controller/LoginController.java
@@ -7,73 +7,77 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.cee.model.Credentials;
 import pl.cee.model.State;
 import pl.cee.model.User;
 import pl.cee.service.SessionIdGenerator;
 
+import javax.cache.expiry.CreatedExpiryPolicy;
+import javax.cache.expiry.Duration;
+import javax.cache.expiry.ExpiryPolicy;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 
 @RequestMapping("/")
 public class LoginController {
 
-    private static final String COOKIE_NAME = "session.id";
+    private static final String SESSION_COOKIE_NAME = "cee.session.id";
+    private static final int SESSION_EXPIRE_MINUTES = 5;
 
-    private final Map<Credentials, User> accounts = new HashMap<>(3);
-    private final SessionIdGenerator sessionIdGenerator;
     private final IgniteCache<String, State> cache;
+    private final SessionIdGenerator sessionIdGenerator;
+    private final Map<Credentials, User> accounts;
 
     public LoginController(Ignite ignite, SessionIdGenerator sessionIdGenerator) {
+        ExpiryPolicy expiryPolicy = new CreatedExpiryPolicy(new Duration(TimeUnit.MINUTES, SESSION_EXPIRE_MINUTES));
+        this.cache = ignite.<String, State>cache("sessionCache").withExpiryPolicy(expiryPolicy);
+        this.sessionIdGenerator = sessionIdGenerator;
+
+        this.accounts = new HashMap<>(3);
         this.accounts.put(new Credentials("admin", "admin"), new User("administrator"));
         this.accounts.put(new Credentials("student", "student"), new User("student"));
         this.accounts.put(new Credentials("mr", "bean"), new User("Mr. Bean"));
-
-        this.sessionIdGenerator = sessionIdGenerator;
-        this.cache = ignite.cache("sessionCache");
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home() {
+    public String homeGet() {
         return "redirect:/login";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(Model model,
-                        @CookieValue(value = COOKIE_NAME, required = false, defaultValue = "") String sessionId) {
+    public String loginGet(@CookieValue(value = SESSION_COOKIE_NAME, defaultValue = "") String sessionId,
+                           Model model) {
         State appState = cache.get(sessionId);
-
-        model.addAttribute("reqState", "none");
         model.addAttribute("appState", appState);
         return "login";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(Model model,
-                        @ModelAttribute Credentials credentials,
-                        HttpServletResponse response) {
+    public String loginPost(@ModelAttribute Credentials credentials,
+                            HttpServletResponse response,
+                            RedirectAttributes redirectAttributes) {
         User user = accounts.get(credentials);
 
         if (user != null) {
             String sessionId = sessionIdGenerator.nextSessionId();
-            State state = new State(user);
-
-            cache.put(sessionId, state);
-            response.addCookie(new Cookie(COOKIE_NAME, sessionId));
+            State appState = new State(user);
+            cache.put(sessionId, appState);
+            response.addCookie(new Cookie(SESSION_COOKIE_NAME, sessionId));
             return "redirect:/login";
 
         } else {
-            model.addAttribute("reqState", "fail");
-            model.addAttribute("appState", null);
-            return "login";
+            redirectAttributes.addFlashAttribute("reqState", "fail");
+            return "redirect:/login";
         }
     }
 
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
-    public String logout(@CookieValue(value = COOKIE_NAME, required = false, defaultValue = "") String sessionId) {
+    public String logoutGet(@CookieValue(value = SESSION_COOKIE_NAME, defaultValue = "") String sessionId) {
         cache.remove(sessionId);
         return "redirect:/login";
     }
