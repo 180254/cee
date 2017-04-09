0c4e9978ee22d1d6574a283c20e560b272695400
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2017-04-09 22:43:59 +0200

    cache: move config-ignite copy job from Vagrantfile to app.sh

--- cache/cache/app.sh
+++ cache/cache/app.sh
@@ -4,6 +4,7 @@ cd "$(dirname -- "$0")"
 
 (cd app/ && ./mvnw clean package -DskipTests=true)
 cp app/target/login-app-0.0.1.jar ./
+cp app/src/main/resources/META-INF/config-ignite.xml ./
 
 vagrant ssh ignite1 -c " pkill -f login-app"
 vagrant ssh ignite1 -c " cp /vagrant/login-app-0.0.1.jar /home/vagrant"
@@ -13,7 +14,9 @@ vagrant ssh ignite1 -c " nohup java -jar -Dserver.port=9001 /home/vagrant/login-
 vagrant ssh ignite2 -c " pkill -f login-app"
 vagrant ssh ignite2 -c " pkill -f ignite"
 vagrant ssh ignite2 -c " cp /vagrant/login-app-0.0.1.jar /home/vagrant"
+vagrant ssh ignite2 -c " cp /vagrant/config-ignite.xml   /home/vagrant"
 vagrant ssh ignite2 -c " nohup java -jar -Dserver.port=9002 /home/vagrant/login-app-0.0.1.jar  > nohup9002.out 2>&1 & sleep 1"
 vagrant ssh ignite2 -c " nohup /usr/local/ignite/bin/ignite.sh /home/vagrant/config-ignite.xml > nohup1000.out 2>&1 & sleep 1"
 
 rm login-app-0.0.1.jar
+rm config-ignite.xml
